1b652579bfef763939c09d1564d72b33cf2d7699
Karl Jonsson <49280110+KarlJJonsson@users.noreply.github.com>
2020-04-29 10:47:30 +0200

    Add files via upload

---
+++ UrbanAdventure/src/main/java/com/group158/UrbanAdventure/UrbanAdventureApplication.java
@@ -0,0 +1,13 @@
+package com.group158.UrbanAdventure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanAdventureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanAdventureApplication.class, args);
+	}
+
+}
---
+++ UrbanAdventure/src/test/java/com/group158/UrbanAdventure/UrbanAdventureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group158.UrbanAdventure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanAdventureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
