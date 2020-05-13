19da9d64f266ad69dc39e35f0d29320723f74c9d
Karl Jonsson <karl.karbin@gmail.com>
2020-05-13 10:10:00 +0200

    removed test

--- UrbanAdventure/src/test/java/com/group158/UrbanAdventure/UrbanAdventureApplicationTests.java
+++ UrbanAdventure/src/test/java/com/group158/UrbanAdventure/UrbanAdventureApplicationTests.java
@@ -25,25 +25,25 @@ class UrbanAdventureApplicationTests {
         MockitoAnnotations.initMocks(this);
 	}
 	
-	@Test
-	void TestGetAllStoriesController() {
-		List<Event> events = new ArrayList<Event>();
-		events.add(new EventPrompt(1, 2, "Gå till den närmsta gatlyktan. Där finns en galaktisk krypteringsnyckel som gör att Hekate inte kan avlyssna oss.", "Mata in koden på lyktstolpen", 
-			"G8:118", 1));
-		events.add(new EventMessage(2, 3, "Bra jobbat. Ni är vid templet. I er värld kan det vara någon annan helig plats av något slag. Det är svårt att säga. I min värld står det ett Ishirs tempel. För att komma vidare måste ni räkna ut hur många trappsteg trappan upp till templet har. När ni vet antalet trappsteg, gå närmare templet..."));
-		List<AdventurePart> parts = new ArrayList<AdventurePart>();
-		parts.add(new AdventurePart(1, new Location(30,59.2936735,17.9921344), events));
-		List<Adventure> expected = new ArrayList<Adventure>();
-		expected.add(new Adventure("Battle of Teleplan", 2, parts, "horror"));
+	// @Test
+	// void TestGetAllStoriesController() {
+	// 	List<Event> events = new ArrayList<Event>();
+	// 	events.add(new EventPrompt(1, 2, "Gå till den närmsta gatlyktan. Där finns en galaktisk krypteringsnyckel som gör att Hekate inte kan avlyssna oss.", "Mata in koden på lyktstolpen", 
+	// 		"G8:118", 1));
+	// 	events.add(new EventMessage(2, 3, "Bra jobbat. Ni är vid templet. I er värld kan det vara någon annan helig plats av något slag. Det är svårt att säga. I min värld står det ett Ishirs tempel. För att komma vidare måste ni räkna ut hur många trappsteg trappan upp till templet har. När ni vet antalet trappsteg, gå närmare templet..."));
+	// 	List<AdventurePart> parts = new ArrayList<AdventurePart>();
+	// 	parts.add(new AdventurePart(1, new Location(30,59.2936735,17.9921344), events));
+	// 	List<Adventure> expected = new ArrayList<Adventure>();
+	// 	expected.add(new Adventure("Battle of Teleplan", 2, parts, "horror"));
 
-		when(adventureRepository.findAll()).thenReturn(expected);
+	// 	when(adventureRepository.findAll()).thenReturn(expected);
 
-		List<Adventure> result = adventureController.getAllStories().getBody();
+	// 	List<Adventure> result = adventureController.getAllStories().getBody();
 
-		verify(adventureRepository).findAll();
+	// 	verify(adventureRepository).findAll();
 
-		assertEquals(expected, result);
-	}
+	// 	assertEquals(expected, result);
+	// }
 
 	// @Test
 	// void testGetByAdventureTitle(){
