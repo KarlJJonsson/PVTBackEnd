d9ba545adab9c9a6fd6a92eb7d91d114203e7a8a
Jimmie Jonasson <jimmie.me.jonasson@gmail.com>
2020-05-20 22:57:11 +0200

    Basic Security intro...

---
+++ UrbanAdventure/src/main/java/com/group158/UrbanAdventure/auth/User.java
@@ -0,0 +1,15 @@
+package com.group158.UrbanAdventure.auth;
+
+import org.springframework.data.mongodb.core.index.IndexDirection;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+public class User{
+
+
+
+}
