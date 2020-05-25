75ac28f23149f2fe5e0672e66b1bd9c58e4bfe4d
Karl Jonsson <karl.karbin@gmail.com>
2020-05-25 17:58:03 +0200

    EventMedia

---
+++ UrbanAdventure/src/main/java/com/group158/UrbanAdventure/EventMedia.java
@@ -0,0 +1,52 @@
+package com.group158.UrbanAdventure;
+
+import java.util.Objects;
+
+public class EventMedia extends Event {
+
+    private String image;
+    private String sound;
+    private String video;
+
+    public EventMedia(int path, int index, String image, String sound, String video) {
+        super(index, path);
+        this.image = image;
+        this.sound = sound;
+        this.video = video;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSound() {
+        return this.sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+
+    public String getVideo() {
+        return this.video;
+    }
+
+    public void setVideo(String video) {
+        this.video = video;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof EventMedia)) {
+            return false;
+        }
+        EventMedia eventMedia = (EventMedia) o;
+        return Objects.equals(image, eventMedia.image) && Objects.equals(sound, eventMedia.sound) && Objects.equals(video, eventMedia.video);
+    }
+}
